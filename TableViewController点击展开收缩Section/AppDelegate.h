1570bb92b7285a22fae06b36b10187899f815e6e
cjq002 <chenjiaqing@Mac-mini-CJQ.local>
2015-02-06 16:44:10 +0800

    Initial Commit

---
+++ TableViewController点击展开收缩Section/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewController点击展开收缩Section
+//
+//  Created by 陈家庆 on 15-2-6.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewController点击展开收缩Section/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewController点击展开收缩Section
+//
+//  Created by 陈家庆 on 15-2-6.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
